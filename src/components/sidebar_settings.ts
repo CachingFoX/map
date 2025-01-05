084fbca8fb94f2444d4b059207934359596ea19b
CachingFoX <cachingfox@cachingfox.org>
2025-01-05 16:36:38 +0100

    new sidebar tab "Settings"

---
+++ src/components/sidebar_settings.ts
@@ -0,0 +1,130 @@
+import {App} from "./app";
+import {MapState, MapStateChange} from "./map_state";
+import {SidebarItem} from "./sidebar_item";
+import {create_element} from "./utilities";
+import {CoordinatesFormat, parseCoordinatesFormat} from "./coordinates";
+import {Distance, DistanceFormat, parseDistanceFormat} from "./distance";
+
+interface ICoordinatesFormatDict {
+    id: string;
+    name: string;
+}
+
+interface IDistanceFormatDict {
+    id: DistanceFormat;
+    name: string;
+}
+
+export class SidebarSettings extends SidebarItem {
+    private readonly language_select: HTMLInputElement;
+    private readonly coordinates_format_select: HTMLInputElement;
+    private readonly units_format_select: HTMLInputElement;
+    private readonly centerField: HTMLParagraphElement;
+    private readonly distanceField: HTMLParagraphElement;
+
+    public constructor(app: App, id: string) {
+        super(app, id);
+
+        this.language_select = this._div.querySelector("[data-language]")!;
+
+        interface ITitleShort {
+            title: string;
+            short: string;
+        }
+
+        [
+            {title: "English", short: "en"},
+            {title: "Deutsch", short: "de"},
+            {title: "Français", short: "fr"},
+        ].forEach((language: ITitleShort): void => {
+            this.language_select.append(
+                new Option(
+                    language.title,
+                    language.short,
+                    language.short === "en",
+                    language.short === this.app.map_state.language,
+                ),
+            );
+        });
+        this.language_select.onchange = (): void => {
+            this.app.map_state.set_language(this.language_select.value);
+        };
+
+        this.coordinates_format_select = this._div.querySelector("[data-coordinates-format]")!;
+        [
+            {id: CoordinatesFormat.D, name: "Degrees"},
+            {id: CoordinatesFormat.DM, name: "Degrees+Minutes"},
+            {id: CoordinatesFormat.DMS, name: "Degrees+Minutes+Seconds"},
+        ].forEach((item: ICoordinatesFormatDict): void => {
+            this.coordinates_format_select.append(
+                new Option(
+                    item.name,
+                    item.id,
+                    item.id === CoordinatesFormat.DM,
+                    item.id === this.app.map_state.settings_marker_coordinates_format,
+                ),
+            );
+        });
+        this.coordinates_format_select.onchange = (): void => {
+            this.app.map_state.set_coordinates_format(parseCoordinatesFormat(this.coordinates_format_select.value, CoordinatesFormat.DM));
+        };
+
+        this.units_format_select = this._div.querySelector("[data-distance-format]")!;
+        [
+            {id: DistanceFormat.m, name: "m"},
+            {id: DistanceFormat.km, name: "km"},
+            {id: DistanceFormat.ft, name: "ft"},
+            {id: DistanceFormat.mi, name: "mi"},
+        ].forEach((item: IDistanceFormatDict): void => {
+            this.units_format_select.append(
+                new Option(
+                    item.name,
+                    item.id,
+                    item.id === DistanceFormat.m,
+                    item.id === this.app.map_state.settings_line_distance_format,
+                ),
+            );
+        });
+        this.units_format_select.onchange = (): void => {
+            this.app.map_state.set_distance_format(parseDistanceFormat(this.units_format_select.value, DistanceFormat.m))
+        };
+
+        this.centerField = this._div.querySelector("#sidebar-settings-center")!;
+        this.distanceField = this._div.querySelector("#sidebar-settings-distance")!;
+    }
+
+    public update_state(changes: number, _marker_id: number = -1): void {
+        console.log(changes, _marker_id);
+        // if ((changes & (MapStateChange.MARKERS | MapStateChange.LINES | MapStateChange.CENTER | MapStateChange.LANGUAGE)) === MapStateChange.NOTHING) {
+        //    return;
+        // }
+
+        if (changes & MapStateChange.LANGUAGE) {
+            this.language_select.value = this.app.map_state.language;
+        }
+        if (changes & (MapStateChange.MARKERS | MapStateChange.CENTER)) {
+            this.coordinates_format_select.value = this.app.map_state.settings_marker_coordinates_format;
+            this.centerField.innerText =
+                (this.app.map_state.center === null) ?
+                "n/a" :
+                this.app.map_state.center.to_string(this.app.map_state.settings_marker_coordinates_format);
+        }
+        if (changes & (MapStateChange.LINES | MapStateChange.VIEW | MapStateChange.CENTER | MapStateChange.ZOOM)) {
+            this.units_format_select.value = this.app.map_state.settings_line_distance_format;
+            
+            var d = this.app.leaflet.distance_swne();
+            var diagonal = `${d.to_string(this.app.map_state.settings_line_distance_format)}`
+
+            if (this.app.map_state.settings_line_distance_format != DistanceFormat.m && this.app.map_state.settings_line_distance_format != DistanceFormat.km) {
+                var metric = d.to_string(DistanceFormat.m);
+                if (d.m() >= 100000) {
+                    metric = d.to_string(DistanceFormat.km)
+                }
+                diagonal += ` (${metric})` 
+            }
+            this.distanceField.innerText = diagonal
+            
+        }        
+    }
+
+}
