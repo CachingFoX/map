63844cd33ba17983e8b351067cd14cf5a0a706a1
CachingFoX <cachingfox@cachingfox.org>
2025-01-21 21:49:35 +0100

    remove script

--- tools/update-translations.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-echo de
-print -l ./src/**/*.ts | xargs python3 ./tools/find-i18n.py -t ./src/lang/de/translation.json ./src/index.html
-echo en
-print -l ./src/**/*.ts | xargs python3 ./tools/find-i18n.py -t ./src/lang/en/translation.json ./src/index.html
-echo fr
-print -l ./src/**/*.ts | xargs python3 ./tools/find-i18n.py -t ./src/lang/fr/translation.json ./src/index.html
